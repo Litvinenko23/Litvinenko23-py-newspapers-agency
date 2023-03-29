48f3a49cc14c984acb0497d58f90c9f5e93010dc
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-03-29 12:45:28 +0300

    Added initial migration

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from newspaper.models import Topic, Redactor, Newspaper
+
+admin.site.register(Topic)
+admin.site.register(Redactor)
+admin.site.register(Newspaper)
