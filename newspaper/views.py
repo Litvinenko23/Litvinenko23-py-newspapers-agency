bedea7f133f1c94a5a440832c5242b73d4c148a7
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-04-03 18:04:36 +0300

    Updated settings & requirements.txt

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
